bb6c310f0c2fb9938d90be2b143f4eac11dc89c2
raminkarim <ramin.karim@emaintenancelab.com>
2023-01-24 22:22:55 +0100

    v0.1

--- hello world.py
+++ hello world.py
@@ -1,5 +1,9 @@
+from ast import For
+
+
 x = 1
 exit_msg = "Bye!"
+tbl01=["a", "b", "c"] ,["10","20","30"]
 if x == 1:
     # indented four spaces
     print("Hello World ::)")
@@ -7,4 +11,11 @@ if x == 1:
 else:
     print("I'm here :((")
 
-print(exit_msg)
+print(exit_msg + '>>>' + input("Enter an string:"))
+for i in range(3):
+    print(tbl01[0][i])
+
+
+
+
+#print(tbl01[0][0],[1][0])l
